9fb9f951b535c78aba798df10c421799152305aa
Lewis Parmenter <97650662+lewpar@users.noreply.github.com>
2026-06-05 21:18:16 +1000

    Added drive state to LED matrix and improved tilt range

--- samples/car-controller/controller.py
+++ samples/car-controller/controller.py
@@ -5,23 +5,21 @@ radio.on()
 
 while True:
     y = accelerometer.get_y()
-
-    if y < 400:
-        print("Sending forward command..")
+    
+    if y < 100:
         radio.send("forward")
-    elif y > 900:
-        print("Sending backward command..")
+        display.show(Image.ARROW_N)
+    elif y > 700:
         radio.send("backward")
+        display.show(Image.ARROW_S)
     else:
-        print("Sending stop command..")
         radio.send("stop")
+        display.show(Image.SQUARE_SMALL)
 
     left_message = "left_" + ("on" if button_a.is_pressed() else "off")
     right_message = "right_" + ("on" if button_b.is_pressed() else "off")
+    
     radio.send(left_message)
     radio.send(right_message)
-
-    print(left_message)
-    print(right_message)
 
     sleep(50)
